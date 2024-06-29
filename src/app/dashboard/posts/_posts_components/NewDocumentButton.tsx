c4bcffcf65ec70c2e9be40c97b46c68c28c64095
matéo! <6matbub@gmail.com>
2024-06-29 03:14:09 -0700

    chore: rm alerts

--- src/app/dashboard/posts/_posts_components/NewDocumentButton.tsx
+++ src/app/dashboard/posts/_posts_components/NewDocumentButton.tsx
@@ -2,102 +2,107 @@
 
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogActions,
-  DialogBody,
-  DialogDescription,
-  DialogTitle,
+	Dialog,
+	DialogActions,
+	DialogBody,
+	DialogDescription,
+	DialogTitle,
 } from "@/components/ui/dialogV2";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { formatSlug } from "@/utils/formatSlug";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { toast } from "@/components/ui/use-toast";
 
 export default function NewDocumentButton({
-  url = "chroniconl.com",
+	url = "chroniconl.com",
 }: { url: string }) {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [inputValue, setInputValue] = useState("");
+	const [modalOpen, setModalOpen] = useState(false);
+	const [inputValue, setInputValue] = useState("");
 
-  const { control, handleSubmit, watch, register } = useForm({
-    defaultValues: {
-      title: "",
-      url: "",
-    },
-  });
+	const { control, handleSubmit, watch, register } = useForm({
+		defaultValues: {
+			title: "",
+			url: "",
+		},
+	});
 
-  const createNewPost = async (data: any) => {
-    const title = data.title;
-    const url = data.url;
+	const createNewPost = async (data: any) => {
+		const title = data.title;
+		const url = data.url;
 
-    try {
-      const response = await fetch("/api/v0/document/create", {
-        method: "POST",
-        body: JSON.stringify({ title, url }),
-      });
-      const { data: slug, error, message } = await response.json();
+		try {
+			const response = await fetch("/api/v0/document/create", {
+				method: "POST",
+				body: JSON.stringify({ title, url }),
+			});
+			const { data: slug, error, message } = await response.json();
 
-      if (error) {
-        alert(message);
-        return;
-      }
+			if (error) {
+				toast({
+					title: "Error",
+					description: "Failed to create document",
+					variant: "destructive",
+				});
+				return;
+			}
 
-      window.location.href = `/dashboard/posts/${slug}/edit`;
-    } catch (error) {
-      return;
-    }
-  };
+			window.location.href = `/dashboard/posts/${slug}/edit`;
+		} catch (error) {
+			return;
+		}
+	};
 
-  const urlPreviewValue =
-    "https://" + url + "/" + formatSlug(watch("title", ""));
+	const urlPreviewValue =
+		"https://" + url + "/" + formatSlug(watch("title", ""));
 
-  return (
-    <>
-      <Button onClick={() => setModalOpen(true)} className="h-fit">
-        Create Post
-      </Button>
-      <Dialog open={modalOpen} onClose={() => setModalOpen(false)}>
-        <DialogTitle>Create New Post</DialogTitle>
-        <DialogDescription></DialogDescription>
-        <DialogBody>
-          <form
-            className="flex flex-col gap-4"
-            onSubmit={handleSubmit(createNewPost)}
-          >
-            <div>
-              <Label htmlFor="title">Title</Label>
-              <Controller
-                name="title"
-                control={control}
-                defaultValue={inputValue}
-                render={({ field }: any) => (
-                  <Input
-                    {...field}
-                    type="text"
-                    placeholder="Enter the title of the document"
-                  />
-                )}
-              />
-            </div>
+	return (
+		<>
+			<Button onClick={() => setModalOpen(true)} className="h-fit">
+				Create Post
+			</Button>
+			<Dialog open={modalOpen} onClose={() => setModalOpen(false)}>
+				<DialogTitle>Create New Post</DialogTitle>
+				<DialogDescription></DialogDescription>
+				<DialogBody>
+					<form
+						className="flex flex-col gap-4"
+						onSubmit={handleSubmit(createNewPost)}
+					>
+						<div>
+							<Label htmlFor="title">Title</Label>
+							<Controller
+								name="title"
+								control={control}
+								defaultValue={inputValue}
+								render={({ field }: any) => (
+									<Input
+										{...field}
+										type="text"
+										placeholder="Enter the title of the document"
+									/>
+								)}
+							/>
+						</div>
 
-            <div>
-              <Label htmlFor="url">Url Preview</Label>
-              <Input
-                type="text"
-                name="url"
-                id="url"
-                placeholder={url || "https://"}
-                defaultValue={urlPreviewValue}
-                disabled
-              />
-            </div>
+						<div>
+							<Label htmlFor="url">Url Preview</Label>
+							<Input
+								type="text"
+								name="url"
+								id="url"
+								placeholder={url || "https://"}
+								defaultValue={urlPreviewValue}
+								disabled
+							/>
+						</div>
 
-            <Button type="submit">Create Post</Button>
-            <Button onClick={() => setModalOpen(false)}>Cancel</Button>
-          </form>
-        </DialogBody>
-      </Dialog>
-    </>
-  );
+						<Button type="submit">Create Post</Button>
+						<Button onClick={() => setModalOpen(false)}>Cancel</Button>
+					</form>
+				</DialogBody>
+			</Dialog>
+		</>
+	);
 }
