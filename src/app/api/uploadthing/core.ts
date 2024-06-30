86c548e7ef86abb6a2981fd87be0464927aa59ff
matéo! <6matbub@gmail.com>
2024-06-30 05:36:44 -0700

    fix: temp solution for image management
    going to migrate away from uploadthing because it causes a lot of build errors

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,41 +1,51 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { UploadThingError } from 'uploadthing/server'
 import { auth, currentUser } from '@clerk/nextjs/server'
+import { supabase } from '@/utils/supabase'
 
 const f = createUploadthing()
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({
-    image: {
-      maxFileSize: '4MB',
-      maxFileCount: 1,
-    },
-  })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      const user = await currentUser()
-      if (!user) {
-        return { userId: null }
-      }
-      // This code runs on your server before upload
-      const { userId } = auth()
+	// Define as many FileRoutes as you like, each with a unique routeSlug
+	imageUploader: f({
+		image: {
+			maxFileSize: '4MB',
+			maxFileCount: 1,
+		},
+	})
+		// Set permissions and file types for this FileRoute
+		.middleware(async ({ req }) => {
+			const { userId } = auth()
 
-      // If you throw, the user will not be able to upload
-      if (!userId) throw new UploadThingError('Unauthorized')
+			// If you throw, the user will not be able to upload
+			if (!userId) throw new UploadThingError('Unauthorized')
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: userId }
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      // !!! Whatever is returned here is sent to the client side `onClientUploadComplete` callback
-      return {
-        uploadedBy: metadata.userId,
-        url: file.url,
-      }
-    }),
+			// Whatever is returned here is accessible in onUploadComplete as `metadata`
+			return { userId: userId }
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			const { error } = await supabase
+				.from('photo_refs')
+				.insert({
+					id: file.key,
+					name: file.name,
+					image_url: file.url,
+					image_key: file.key,
+				})
+
+			if (error) {
+				console.error(error)
+				return
+			}
+
+			// This code RUNS ON YOUR SERVER after upload
+			// !!! Whatever is returned here is sent to the client side `onClientUploadComplete` callback
+			return {
+				uploadedBy: metadata.userId,
+				url: file.url,
+			}
+		}),
 } satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
