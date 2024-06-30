647cbae736d2951c1481757c5e882bb80e62d810
matéo! <6matbub@gmail.com>
2024-06-30 07:44:59 -0700

    feat: present media, rm MediaUploadForm

--- src/app/dashboard/media/_media_components/MediaView.tsx
+++ src/app/dashboard/media/_media_components/MediaView.tsx
@@ -1,53 +1,44 @@
+import { ClientImage } from '@/components/ui/image'
 import { Input } from '@/components/ui/input'
 import { SearchIcon } from 'lucide-react'
+import { Text } from '@/components/ui/text'
+import { Heading } from '@/components/ui/heading'
 
-export default function MediaView() {
+export default function MediaView({ media }: { media: {
+	id: string
+	name: string
+	image_url: string
+	image_key: string
+	size: number
+}[] }) {
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex items-center gap-2 px-4 py-6">
-        <SearchIcon className="h-5 w-5 text-muted-foreground" />
-        <Input
-          className="w-full"
-          placeholder="Search images..."
-          type="search"
-        />
-      </div>
       <div className="space-y-2 px-4">
-        <h2 className="text-2xl font-bold">Media Gallery</h2>
-        <p className="text-stone-500 dark:text-stone-400">
+        <Heading level={2}>Media Gallery</Heading>
+        <Text>
           Here you can view all the images you have uploaded to your account.
-        </p>
+        </Text>
       </div>
       <div className="mb-12 grid grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3">
-        {
-          // array with 10 items
-          Array.from({ length: 10 }).map((_, index) => (
+        {media && media.map(({ id, name, image_url, size }) => (
             <div
-              key={index}
+              key={id}
               className="group flex flex-col gap-2 rounded-md border border-stone-200 bg-white p-2 shadow-sm hover:shadow-md dark:border-stone-800 dark:bg-stone-950"
             >
-              <div className="relative aspect-video overflow-hidden rounded-md">
-                {/* <img
-                  alt="Image Preview"
-                  className="object-cover transition-all group-hover:scale-110"
-                  height={300}
-                  src="https://source.unsplash.com/random/?Zoo,Animals"
-                  style={{
-                    aspectRatio: "500/300",
-                    objectFit: "cover",
-                  }}
-                  width={500}
-                /> */}
-              </div>
+							<ClientImage 
+								src={image_url} 
+								alt={name}
+							/>
               <div className="flex items-center justify-between gap-2">
-                <div className="line-clamp-1 text-sm font-medium">
-                  image_{index}.jpg
-                </div>
-                <div className="text-xs text-muted-foreground">1.2 MB</div>
+                <Text small className="line-clamp-1 text-sm font-medium">
+                  {name}
+                </Text>
+                <Text small className='whitespace-nowrap'>
+									{(size / 1000000).toFixed(2)} MB
+								</Text>
               </div>
             </div>
-          ))
-        }
+          ))}
       </div>
     </div>
   )
