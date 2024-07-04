146c68dfd84f5d4b9273f31592e4464327925d1a
matéo! <6matbub@gmail.com>
2024-07-04 07:07:00 -0700

    feat: update publish status based on if post is published

--- src/app/dashboard/posts/_posts_components/PostCard.tsx
+++ src/app/dashboard/posts/_posts_components/PostCard.tsx
@@ -108,7 +108,7 @@ export default function PostCard({
 							<>
 								<div className="col-span-3">
 									<Text small>
-										{'Published '}
+										{isPublished ? 'Published' : 'Set to publish '}
 									</Text>
 								</div>
 								<div className="col-span-9">
