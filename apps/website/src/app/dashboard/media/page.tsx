0d83152cfe93208b2974fab7ef4f8d271eda2f97
matéo! <6matbub@gmail.com>
2024-07-07 06:41:54 -0700

    fix: subtle failures in prod build

--- apps/website/src/app/dashboard/media/page.tsx
+++ apps/website/src/app/dashboard/media/page.tsx
@@ -2,38 +2,43 @@ import MediaView from '@/app/dashboard/media/_media_components/MediaView'
 import { supabase } from '@/utils/supabase'
 
 const fetchData = async () => {
-	if (!process.env.UPLOADTHING_SECRET) {
+	try {
+		if (!process.env.UPLOADTHING_SECRET) {
+			return
+		}
+
+		const response = await fetch('https://uploadthing.com/api/listFiles', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Uploadthing-Api-Key': process.env.UPLOADTHING_SECRET,
+				'X-Uploadthing-Version': '6.4.0'
+			},
+			body: JSON.stringify({
+
+			})
+		})
+
+		if (!response.ok) {
+			throw new Error('Failed to fetch media')
+		}
+
+		const data = await response.json()
+
+		return data
+	}
+	catch (error) {
+		console.error(error)
 		return
 	}
-
-	const response = await fetch('https://uploadthing.com/api/listFiles', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-Uploadthing-Api-Key': process.env.UPLOADTHING_SECRET,
-			'X-Uploadthing-Version': '6.4.0'
-		},
-		body: JSON.stringify({
-		
-		})
-	})
-
-	if (!response.ok) {
-		throw new Error('Failed to fetch media')
-	}
-
-	const data = await response.json()
-
-	return data
-	
 }
 
 export default async function Media() {
 	const data = await fetchData()
 
-  return (
-    <section>
+	return (
+		<section>
 			<MediaView media={data?.files} />
-    </section>
-  )
+		</section>
+	)
 }
