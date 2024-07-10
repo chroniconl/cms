8eed5684abb21a48e0542b6c75078837f6aa9bff
matéo! <6matbub@gmail.com>
2024-07-09 21:31:59 -0700

    fix: add missing route

---
+++ src/app/api/v0.1/document/route.ts
@@ -0,0 +1,20 @@
+import { failResponse, okResponse } from '@/utils/response'
+import { supabase } from '@/utils/supabase'
+
+export async function PUT(request: Request) {
+	const requestData = await request.json()
+	const { error } = await supabase
+		.from('posts')
+		.update({
+			title: requestData.title,
+			content: requestData.content,
+		})
+		.match({ slug: requestData.slug })
+
+	if (error) {
+		console.error(error)
+		return failResponse(error?.message)
+	}
+
+	return okResponse('Document updated')
+}
