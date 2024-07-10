fb13fbb75196e241da79fdd06445cfdf221bfa49
matéo! <6matbub@gmail.com>
2024-07-09 18:56:55 -0700

    feat: add desktop nav

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -2,17 +2,17 @@ import Header from '@/components/header/Header'
 import Footer from '@/components/Footer'
 
 export default async function PublicLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <div>
-      <Header />
-      <main className="mx-auto mb-20 mt-8 max-w-7xl px-2 md:mt-10 md:px-4">
-        {children}
-      </main>
-      <Footer />
-    </div>
-  )
+	return (
+		<div>
+			<Header />
+			<main className="mx-auto mb-20 mt-8 max-w-7xl px-2 md:mt-10 md:px-6">
+				{children}
+			</main>
+			<Footer />
+		</div>
+	)
 }
