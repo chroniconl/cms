9a5600798186974337fb54b65767a0b5f90a9f8e
matéo! <6matbub@gmail.com>
2024-07-17 22:15:40 -0700

    CHR-33: Tags that already exists aren't being assigned to the post

--- apps/chroniconl/src/app/api/v0.1/document/filterable-data/route.ts
+++ apps/chroniconl/src/app/api/v0.1/document/filterable-data/route.ts
@@ -1,89 +1,93 @@
 /**
- * this file is kind of muddy
- *
- * but it works
+ * tags are crazy 
  */
 import { getCurrentUser } from '@/server/getCurrentUser'
-import { toPST } from '@/utils/dates'
-import { formatSlug } from '@/utils/formatSlug'
 import { failResponse, okResponse } from '@/utils/response'
 import { supabase } from '@/utils/supabase'
 import { tagSeparator } from '@/utils/tagSeparator'
-import joi from 'joi'
+import { 
+	validateIncomingData, 
+	findTagsThatAlreadyExistInDatabase, 
+	createTagsInDatabase, 
+	getTagsThatAlreadyExistInDatabase, 
+	createTagsRelationshipsInDatabase
+} from './__queries/filterable-data'
 
 export async function POST(request: Request) {
   const { data: userData, error: userError } = await getCurrentUser()
   if (userError) {
     return failResponse('Trouble getting user')
   }
-
+	
   const requestData = await request.json()
-
-  const schema = joi.object({
-    id: joi.string().required(),
-    category_id: joi.string().allow('').optional(),
-    tags: joi.string().optional(),
-  })
-
-  const { error: validationError } = schema.validate(requestData)
-
-  if (validationError) {
-    return failResponse(validationError.message)
-  }
+	const validationError = validateIncomingData(requestData)
+	if (validationError) {
+		return failResponse(validationError)
+	}
 
   // parse tags
   const tags: string[] = tagSeparator(requestData.tags)
 
-  // find the tags that already exist in the database
-  const { data: tagsThatExistData, error: tagsThatExistError } = await supabase
-    .from('tags')
-    .select('id, name')
-    .in('name', tags)
-
-  if (tagsThatExistError) {
-    return failResponse(tagsThatExistError?.message)
-  }
+	const { data: tagsThatExistData, error: tagsThatExistError } = await findTagsThatAlreadyExistInDatabase(tags)
+	if (tagsThatExistError) {
+		return failResponse(tagsThatExistError?.message)
+	}
 
   // Remove any tags that already exist in the database
   const tagsToInsert = tags.filter(
-    (tag) => !tagsThatExistData.find((tagData) => tagData.name === tag),
+    (tag) => !tagsThatExistData?.find((tagData) => tagData.name === tag),
   )
 
-  // create tags in database
-  const { data: tagsData, error: tagsError } = await supabase
-    .from('tags')
-    .upsert(
-      tagsToInsert.map((tag) => ({
-        name: tag,
-        slug: formatSlug(tag),
-        last_updated: toPST(new Date()),
-        created_by: userData?.id,
-      })),
-    )
-    .select()
+	// create tags in database using the filtered (new) tags array 
+	const { data: newTagsData, error: newTagsError } = await createTagsInDatabase(tagsToInsert, userData)
+	if (newTagsError) {
+		return failResponse(newTagsError?.message)
+	}
 
-  if (tagsError) {
-    return failResponse(tagsError?.message)
-  }
+	// TODO: TEST that we got the tags back at this point
+	console.log('newTagsData', newTagsData)
+	
+	// find the tags that already exist in the database
+	const tagsToRetrieveFromDatabase = tags.filter(
+		(tag) => tagsThatExistData?.find((tagData) => tagData.name === tag),
+	)
+	
+	// retrieve tags that are already in the database
+	const { data: tagsRetrievedData, error: tagsRetrievedError } = await getTagsThatAlreadyExistInDatabase(tagsToRetrieveFromDatabase)
+	if (tagsRetrievedError) {
+		return failResponse(tagsRetrievedError?.message)
+	}
+	
+	// merge the two arrays
+	const tagsDataMerged = [...newTagsData || [], ...tagsRetrievedData || []]
+
+	// if the post_tag_relationship table already has the tags, we don't need to create them again
+	// otherwise we see duplicate names in the post relationship 
+	const { data: existingPostTagRelationshipData, error: existingPostTagRelationshipError } = await supabase
+		.from('post_tag_relationship')
+		.select('*')
+		.eq('post_id', requestData.id)
+
+	if (existingPostTagRelationshipError) {
+		return failResponse(existingPostTagRelationshipError?.message)
+	}
+
+	const postTagRelationshipDataIds = existingPostTagRelationshipData?.map(({postTagRelationship}) => postTagRelationship.tag_id)
+	
+	// remove the tags that already exist in the post_tag_relationship table
+	const tagsDataMergedWithoutExistingRelationships = tagsDataMerged.filter((tag) => !postTagRelationshipDataIds.includes(tag.id))
 
   // create post_tag_relationship in database
-  const { error: postTagRelationshipError } = await supabase
-    .from('post_tag_relationship')
-    .insert(
-      tagsData.map((tag) => ({
-        tag_id: tag.id,
-        post_id: requestData.id,
-        created_by: userData?.id,
-      })),
-    )
-    .select()
+	const { error: postTagRelationshipError } = await createTagsRelationshipsInDatabase(tagsDataMergedWithoutExistingRelationships, {
+		tagAuthorId: userData?.id,
+		postId: requestData.id,
+	})
 
   if (postTagRelationshipError) {
     return failResponse(postTagRelationshipError?.message)
   }
-
+  
   if (requestData.category_id) {
-    // Update post with new category_id
     const { error: postError } = await supabase
       .from('posts')
       .update({
