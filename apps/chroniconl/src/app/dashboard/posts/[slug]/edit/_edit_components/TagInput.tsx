9d939f48d9b99048916fd5af5a1664c9357b3f01
matéo! <6matbub@gmail.com>
2024-07-18 23:09:44 -0700

    CHR-37: implement subscribe-to newsletter

---
+++ apps/chroniconl/src/app/dashboard/posts/[slug]/edit/_edit_components/TagInput.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { useState } from "react"
+import { Label } from "@repo/ui/label"
+import { Button } from "@repo/ui/button"
+import { Input } from "@repo/ui/input"
+import { Badge } from "@repo/ui/badge"
+import { XIcon } from "lucide-react"
+
+export default function TagInput() {
+  const [tags, setTags] = useState([
+    { id: 1, name: "how-to" },
+    { id: 2, name: "nice" },
+    { id: 3, name: "how-to" },
+  ])
+  const [tagInput, setTagInput] = useState("")
+  const addTag = () => {
+    if (tagInput.trim() !== "") {
+      setTags([...tags, { id: Date.now(), name: tagInput.trim() }])
+      setTagInput("")
+    }
+  }
+  
+	const removeTag = (id: number) => {
+    setTags(tags.filter((tag) => tag.id !== id))
+  }
+
+  return (
+		<div className="space-y-2">
+			<Label htmlFor="tags">Tags</Label>
+			<p className="text-sm text-muted-foreground">
+				Tags are used to help categorize your post. They are comma separated.
+			</p>
+			<Input
+				id="tag-input"
+				placeholder="Add a tag"
+				value={tagInput}
+				onChange={(e) => setTagInput(e.target.value)}
+				onKeyDown={(e) => e.key === "Enter" && addTag()}
+			/>
+			<div className="flex flex-wrap gap-4 mt-2">
+				{tags.map((tag) => (
+					<div key={tag.id} className="flex items-center ch-border-outline bg-accent pl-2">
+						<span className="text-xs">{tag.name}</span>
+						<Button variant="ghost" size="icon" onClick={() => removeTag(tag.id)}>
+							<XIcon className="w-3 h-3" />
+						</Button>
+					</div>
+				))}
+			</div>
+		</div>
+  )
+}
--- apps/chroniconl/src/components/LatestHowToPosts.tsx
+++ apps/chroniconl/src/components/LatestHowToPosts.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link'
-import { Badge } from '@repo/ui/badge'
 import { Heading } from '@/components/heading'
-import { ClientImage } from '@/components/image'
-import { Text } from '@/components/text'
 import { formatTimestampToSlug } from '@/utils/formatTimestampToSlug'
 import { Category } from '@/utils/types'
 import { formatDate } from '@/utils/dates'
+import BorderBottom from '@/components/BorderBottom'
 
 export default async function BlogPostsGroup({
   posts,
@@ -39,13 +37,14 @@ export default async function BlogPostsGroup({
 		<div>
 			<div className="mb-4">
 				<Heading level={2}>Latest How-To Posts</Heading>
-				<Text>
-					Here are the latest how-to posts from the community. If you're looking for a specific post, check out the <Link href="/blog">blog</Link> for more.
-				</Text>
+				<div className="mt-4 mb-4" >
+					<BorderBottom height={1} borderColor="#FFF" />
+				</div>
 			</div>
-			<section className="flex flex-col gap-4 divide-y divide-stone-200/50 dark:divide-stone-700/50">
+			<section className="flex flex-col divide-y divide-stone-200/50 dark:divide-stone-700/50">
 				{posts?.slice(0, 6)?.map((post, i) => (
 					<Link
+						className='px-2 group hover:bg-stone-200 dark:hover:bg-stone-800 pb-4'
 						key={post.id}
 						href={`/blog/${formatTimestampToSlug(post.publish_date_day)}/${
 							post.slug
@@ -55,7 +54,9 @@ export default async function BlogPostsGroup({
 							<div className="mt-4 flex flex-col justify-between">
 								<div>
 									<p className="ch-text">{post.title}</p>
-									<p className="ch-text ch-muted">{post?.author?.display_name && post?.author?.display_name + " · "}{formatDate(post?.publish_date_day)}</p> 									
+									{/* <p className="ch-text ch-muted">{post?.author?.display_name && post?.author?.display_name + " · "}{formatDate(post?.publish_date_day)}</p> 									 */}
+									{/* Theres only 1 author for now, dont need to show the author name */}
+									<p className="ch-text ch-muted">{formatDate(post?.publish_date_day)}</p>
 								</div>
 							</div>
 						</article>
