6c09eef671de4c8a9dd0ad752735c76310f3e1ba
matéo! <6matbub@gmail.com>
2024-07-31 20:51:04 -0700

    feat: rm posthog

--- apps/chroniconl/src/app/providers.tsx
+++ apps/chroniconl/src/app/providers.tsx
@@ -2,19 +2,7 @@
 
 import { ThemeProvider, useTheme } from 'next-themes'
 import { usePathname } from 'next/navigation'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
 import { createContext, useEffect, useRef } from 'react'
-
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  })
-}
-export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
-}
 
 function usePrevious(value: any) {
   const ref = useRef()
@@ -57,13 +45,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
   const previousPathname = usePrevious(pathname)
 
   return (
-    <CSPostHogProvider>
-      <AppContext.Provider value={{ previousPathname }}>
-        <ThemeProvider attribute="class" disableTransitionOnChange>
-          <ThemeWatcher />
-          {children}
-        </ThemeProvider>
-      </AppContext.Provider>
-    </CSPostHogProvider>
+		<AppContext.Provider value={{ previousPathname }}>
+			<ThemeProvider attribute="class" disableTransitionOnChange>
+				<ThemeWatcher />
+				{children}
+			</ThemeProvider>
+		</AppContext.Provider>
   )
 }
