9bfe04fabe770c1ff33525e203184de295c60b27
matéo! <6matbub@gmail.com>
2024-08-17 21:32:26 -0700

    fix: error logging to supabase (CHR-94)

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -86,6 +86,7 @@ class Logger {
   async logPerformance(performanceData: Omit<LogData, 'log_level'>) {
     const logData: LogData = {
       ...performanceData,
+      message: '',
       log_level: 'PERFORMANCE',
     }
 
