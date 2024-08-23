2a2f3873aa93fe6c55f9a7d69ad8beeaca880d7e
matéo! <6matbub@gmail.com>
2024-08-22 19:40:15 -0700

    feat: stage for lazy load (CHR-109)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,31 @@ export default async function Page() {
   const categories = await getAllPostCategories()
   return (
     <PublicLayout>
-      <BlogPostsGroup categories={categories} posts={posts} limit={6} />
+      <div className="mb-8 mt-20">
+        <div className="max-w-xl">
+          <h2 className="mb-4 text-2xl font-semibold text-gray-900 dark:text-white sm:text-3xl md:text-4xl">
+            Writings about experiences working with technology
+          </h2>
+          <p className="mb-8">
+            There's always something to be built and there are always something
+            learn. Really I don't know what else to say, so I'll leave it there.
+          </p>
+        </div>
+      </div>
+      <BlogPostsGroup posts={posts.slice(0, 6)} />
       <SubscribeToNewsletter />
+      <div className="mb-8 mt-20">
+        <div className="max-w-xl">
+          <h2 className="mb-4 text-2xl font-semibold text-gray-900 dark:text-white sm:text-3xl md:text-4xl">
+            So like, what does Chroniconl even do?
+          </h2>
+          <p className="mb-8">
+            To put simply; builds web applications and documents the fuck ups
+            along the way.
+          </p>
+        </div>
+      </div>
+      <BlogPostsGroup posts={posts.slice(6, posts.length - 1)} noImage />
     </PublicLayout>
   )
 }
--- src/components/BlogPostsGroup.tsx
+++ src/components/BlogPostsGroup.tsx
@@ -8,8 +8,8 @@ import { Category } from '@/utils/types'
 
 export default async function BlogPostsGroup({
   posts,
-  limit,
-  categories = [],
+  noImage = false,
+  // enableLazyScroll = false,
 }: {
   limit?: number | undefined
   posts: {
@@ -31,45 +31,13 @@ export default async function BlogPostsGroup({
       twitter_handle: string
     }
   }[]
-  categories?: {
-    id: string
-    name: string
-    color: string
-    slug: string
-  }[]
+  noImage?: boolean
+  // enableLazyScroll?: boolean
 }) {
-  const postsCollection = limit ? posts?.slice(0, limit) : posts
   return (
-    <div className="mt-20">
-      <div className="mb-8 grid grid-cols-12">
-        <div className="md:col-span-5">
-          <h2 className="mb-4 text-2xl font-semibold text-gray-900 dark:text-white sm:text-3xl md:text-4xl">
-            Writings about experiences working with technology
-          </h2>
-          <p className="mb-8">
-            There's always something to be built and there are always something
-            learn. I should probably say more, but I'd rather say less.
-          </p>
-        </div>
-
-        <div className="col-span-12 flex flex-wrap gap-2 py-4">
-          {categories &&
-            categories.length > 1 &&
-            categories.map((cat) => (
-              <Badge
-                // @ts-ignore
-                variant={cat.color}
-                size="lg"
-                className="my-0.5"
-                key={cat.id}
-              >
-                {cat.name}
-              </Badge>
-            ))}
-        </div>
-      </div>
+    <div>
       <section className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {postsCollection?.map((post, i) => (
+        {posts?.map((post) => (
           <Link
             className="inline-block h-full"
             key={post.id}
@@ -78,7 +46,9 @@ export default async function BlogPostsGroup({
             }`}
           >
             <article key={post.id} className="group h-full">
-              <ClientImage src={post.image_url} alt={post.image_alt} />
+              {noImage === false && (
+                <ClientImage src={post.image_url} alt={post.image_alt} />
+              )}
               <div className="mt-4 flex flex-col justify-between">
                 <div>
                   <Heading level={3}>{post.title}</Heading>
