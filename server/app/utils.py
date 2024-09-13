4c812d5ec1f136f1d9db1f6be7c49e11b2deeb64
matéo! <6matbub@gmail.com>
2024-09-12 19:25:49 -0700

    earthquake endpoint

--- server/app/utils.py
+++ server/app/utils.py
@@ -4,3 +4,10 @@ from bs4 import BeautifulSoup
 def clean_html(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     return soup.prettify()
+
+def is_california_location(location: str) -> bool:
+    california_names = [" CA", " California", " Calif.", " Cali", " Cal"]
+    # Convert location to lowercase for case-insensitive comparison
+    location_lower = location.lower()
+    # Check if any of the California names are in the location string
+    return any(name.lower() in location_lower for name in california_names)
